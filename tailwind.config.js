217d3fde6b127dbd7eed49ed3345fc829a7b6f37
mpace <mpace@nuveminc.com>
2024-11-22 13:29:03 -0800

    chore: clean-up

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,11 +11,11 @@ export default {
   		center: 'true'
   	},
   	extend: {			
-  		borderRadius: {
-  			lg: 'var(--radius)',
-  			md: 'calc(var(--radius) - 2px)',
-  			sm: 'calc(var(--radius) - 4px)'
-  		},
+  		// borderRadius: {
+  		// 	lg: 'var(--radius)',
+  		// 	md: 'calc(var(--radius) - 2px)',
+  		// 	sm: 'calc(var(--radius) - 4px)'
+  		// },
   		colors: {
   			background: 'hsl(var(--background))',
   			foreground: 'hsl(var(--foreground))',

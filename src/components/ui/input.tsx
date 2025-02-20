9ffa35dab169cbd64a27635f58690b1b211ec623
mpace <mpace@nuveminc.com>
2025-02-19 18:32:16 -0800

    fix: update ui input with theme

--- src/components/ui/input.tsx
+++ src/components/ui/input.tsx
@@ -1,25 +1,41 @@
-import * as React from "react"
+import * as React from 'react';
 
-import { cn } from "@/lib/utils"
+import { cn } from '@/lib/utils';
 
 export interface InputProps
   extends React.InputHTMLAttributes<HTMLInputElement> {}
 
 const Input = React.forwardRef<HTMLInputElement, InputProps>(
   ({ className, type, ...props }, ref) => {
+    const theme = cn(
+      'bg-gray-850',
+      'ring-0',
+      'ring-gray-850',
+      'dark:bg-gray-850',
+      'dark:text-gray-100',
+      'focus:outline-none',
+      'focus:ring-1',
+      'focus:ring-gray-700',
+      '[&:-webkit-autofill]:[transition-delay:9999s]'
+    );
     return (
       <input
         type={type}
         className={cn(
-          "flex h-10 w-full rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium file:text-foreground placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50",
+          'flex h-10 w-full rounded-md px-3 py-2 text-sm',
+          'file:border-0 file:bg-transparent file:text-sm file:font-medium',
+          'placeholder:text-muted-foreground',
+          // 'focus-visible:outline-none focus-visible:ring-2',
+          'disabled:cursor-not-allowed disabled:opacity-50',
+          theme,
           className
         )}
         ref={ref}
         {...props}
       />
-    )
+    );
   }
-)
-Input.displayName = "Input"
+);
+Input.displayName = 'Input';
 
-export { Input }
+export { Input };

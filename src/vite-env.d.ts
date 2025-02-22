99cc802db1125d838c09fcb1c158a15b9f1b631f
mpace <mpace@nuveminc.com>
2025-02-22 11:05:39 -0800

    feat: add environment variables

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,2 +1,14 @@
 /// <reference types="vite/client" />
 /// <reference types="vite-plugin-svgr/client" />
+
+interface ImportMetaEnv {
+  readonly VITE_API_URL: string;
+  readonly VITE_AUTH_TOKEN: string;
+  readonly VITE_ENV: 'development' | 'production' | 'test';
+  readonly VITE_SIMULATED_API_LATENCY: number;
+  // Add any other environment variables you need here
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
--- vite.config.ts
+++ vite.config.ts
@@ -3,11 +3,17 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 import svgr from 'vite-plugin-svgr';
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react(), svgr()],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
+export default defineConfig(({ mode }) => {
+  return {
+    plugins: [react(), svgr()],
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, './src'),
+      },
     },
-  },
+    envDir: '.env',
+    define: {
+      __APP_ENV__: JSON.stringify(mode),
+    },
+  };
 });
